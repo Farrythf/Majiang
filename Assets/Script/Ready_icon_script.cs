62e5b5cc8a4fd9ec62f054e58d987e5201c5ed9c
Hongfei <hongfeit@usc.edu>
2020-12-18 17:36:49 -0800

    12/18

---
+++ Assets/Script/Ready_icon_script.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ready_icon_script : MonoBehaviour
+{
+    public Text TT;
+    public void ButtonEvents()
+    {
+        TT.text = "destroy";
+        Destroy(this.gameObject);
+    }
+
+}
